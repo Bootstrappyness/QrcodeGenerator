38c59eb323b3b5739e3624b1dcbc35d096591f39
Bootstrappyness <rajesh918238@gmail.com>
2025-04-21 14:42:36 +0530

    adding the project

---
+++ qrcode-generator-service/src/main/java/com/rajesh/qrcode/qrcode_generator_service/QrCodeGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.qrcode.qrcode_generator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ qrcode-generator-service/src/test/java/com/rajesh/qrcode/qrcode_generator_service/QrCodeGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.qrcode.qrcode_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
